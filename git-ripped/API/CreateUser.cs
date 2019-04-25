4d0a5ba23f2eafc3aa099bff7dd0a28d89a63e79
Zachery Chambers <ZacheryChambers2@gmail.com>
2019-04-25 13:42:11 -0500

    changes

--- git-ripped/API/CreateUser.cs
+++ git-ripped/API/CreateUser.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -27,27 +27,47 @@ namespace gitripped.API
                 Account account = JsonConvert.DeserializeObject<Account>(json.ToString());
                 SqlConnection conn = OpenSqlConnection();
                 //Create the workout in the database with the max + 1 workoutid
-                SqlCommand command = new SqlCommand("INSERT INTO usr.Account(username, email, firstname, lastname, createdatetime) VALUES (@UserName, @Email, @FirstName, @LastName, GetDate());", conn);
+
+                SqlCommand command = new SqlCommand("SELECT * FROM usr.Account WHERE UserName = @UserName OR Email = @Email", conn);
                 command.Parameters.Add("@UserName", System.Data.SqlDbType.Char);
                 command.Parameters["@UserName"].Value = account.UserName;
                 command.Parameters.Add("@Email", System.Data.SqlDbType.Char);
                 command.Parameters["@Email"].Value = account.Email;
-                command.Parameters.Add("@FirstName", System.Data.SqlDbType.Char);
-                command.Parameters["@FirstName"].Value = account.FirstName;
-                command.Parameters.Add("@LastName", System.Data.SqlDbType.Char);
-                command.Parameters["@LastName"].Value = account.LastName;
                 SqlDataReader reader = command.ExecuteReader();
-                reader.Close();
 
-                command = new SqlCommand("INSERT INTO usr.Password (userID, HashedPassword) VALUES ((SELECT UserID FROM usr.Account WHERE username = @UserName), @Password)", conn);
-                command.Parameters.Add("@Password", System.Data.SqlDbType.Char);
-                command.Parameters["@Password"].Value = account.Password;
-                command.Parameters.Add("@UserName", System.Data.SqlDbType.Char);
-                command.Parameters["@UserName"].Value = account.UserName;
-                reader = command.ExecuteReader();
-                reader.Close();
+                if (!reader.HasRows)
+                {
+                    reader.Close();
 
-                return StatusCode(201);
+                    // for storing the new user data
+                    command = new SqlCommand("INSERT INTO usr.Account(username, email, firstname, lastname, createdatetime) VALUES (@UserName, @Email, @FirstName, @LastName, GetDate());", conn);
+                    command.Parameters.Add("@UserName", System.Data.SqlDbType.Char);
+                    command.Parameters["@UserName"].Value = account.UserName;
+                    command.Parameters.Add("@Email", System.Data.SqlDbType.Char);
+                    command.Parameters["@Email"].Value = account.Email;
+                    command.Parameters.Add("@FirstName", System.Data.SqlDbType.Char);
+                    command.Parameters["@FirstName"].Value = account.FirstName;
+                    command.Parameters.Add("@LastName", System.Data.SqlDbType.Char);
+                    command.Parameters["@LastName"].Value = account.LastName;
+                    reader = command.ExecuteReader();
+                    reader.Close();
+
+                    // for storing the new passowrd
+                    command = new SqlCommand("INSERT INTO usr.Password (userID, HashedPassword) VALUES ((SELECT UserID FROM usr.Account WHERE username = @UserName), @Password);", conn);
+                    command.Parameters.Add("@UserName", System.Data.SqlDbType.Char);
+                    command.Parameters["@UserName"].Value = account.UserName;
+                    command.Parameters.Add("@Password", System.Data.SqlDbType.Char);
+                    command.Parameters["@Password"].Value = account.Password;
+                    reader = command.ExecuteReader();
+                    reader.Close();
+
+                    return StatusCode(201);
+                }
+                else
+                {
+                    reader.Close();
+                    return StatusCode(404, "UserName or Email already exists");
+                }
             }
             catch (Exception E)
             {
@@ -59,40 +79,10 @@ namespace gitripped.API
         {
             public string UserName { get; set; }
             public string Email { get; set; }
-            public string FirstName { get; set;}
+            public string FirstName { get; set; }
             public string LastName { get; set; }
             public string Password { get; set; }
             //public DateTime CreateDateTime { get; set; }
-        }
-
-        static int CheckSessionToken(int token, SqlConnection conn)
-        {
-            SqlCommand command = new SqlCommand("Select Active, UserID From usr.SessionToken WHERE SessionToken = @Token", conn);
-            command.Parameters.Add("@Token", System.Data.SqlDbType.Int);
-            command.Parameters["@Token"].Value = token;
-            SqlDataReader reader = command.ExecuteReader();
-            if (reader.HasRows)
-            {
-                reader.Read();
-                bool Active = (bool)reader["Active"];
-                int UserID = (int)reader["UserID"];
-                if (!Active)
-                {
-                    reader.Close();
-                    return -1;
-                }
-                else
-                {
-                    reader.Close();
-                    return UserID;
-                }
-            }
-            else
-            {
-                reader.Close();
-                return -1;
-            }
-
         }
 
         static SqlConnection OpenSqlConnection()
